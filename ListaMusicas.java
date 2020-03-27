fe9aa544706234acd5d9c69f881c03c00b58196e
Luan Reinheimer <luanmr00@gmail.com>
2020-03-27 17:52:13 -0300

    Finalizdo os metodos da classe ListaMusicas.

--- ListaMusicas.java
+++ ListaMusicas.java
@@ -1,140 +1,96 @@
 package projeto_integrador_UCS;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 
-public class ListaMusicas {
-
+@SuppressWarnings("serial")
+public class ListaMusicas implements Serializable {
+	
+	
 	public static Musicas primeiro;
 	public static Musicas ultimo;
 	public static int tamanho;
-
-	public static ArrayList<Musicas> lista = new ArrayList<>();
-	private static ArrayList<Playlists> playlists = new ArrayList<>();
-
+	
+	public static ArrayList<Musicas> lista = new ArrayList<Musicas>();
+	//private static ArrayList<Playlists> playlists = new ArrayList<>();
+		
 	public ListaMusicas(Musicas m) {
-
+	
 	}
-
 	public ListaMusicas(Musicas primeiro2, Musicas m) {
-		// TODO Auto-generated constructor stub
+		
 	}
-
 	public static ArrayList<Musicas> getLista() {
 		return lista;
 	}
 
-	public static ArrayList<Playlists> getPlaylists() {
-		return playlists;
-	}
+//-----------------------------------------------------------	ok
+	//public static void adicionarInicio(Musicas m) {
+	//		lista.add(0, m);
+//	}
+	public static void adicionarMusica(Musicas nomeMusica) {
+		lista.add(nomeMusica);
+}
 
-//-----------------------------------------------------------	
-	public static void adicionarInicio(Musicas m) {
-		lista.add(0, m);
-	}
-
-	public static void adicionarFinal(Musicas m) {
-		lista.add(m);
-	}
-
-	public static void adicionarPlaylists(Playlists nomePlaylist) {
-		playlists.add(nomePlaylist);
-	}
-//----------------------------------------------------------
-	// static public void listarMusicas() {
-	// Collections.sort(lista, new Comparator<Musicas>() {
-
-	// public int compare(Musicas m, Musicas m2){
-	// return m.getNomeMusica().compareTo(m2.getNomeMusica());
-	// }
-	// }
-	// }
-
-//-----------------------------------------------------------------------
-	static public String listarMusicas() {
-
+//------------------Mostra em ordem alfabética-------------------------------
+	static public String  listarMusicas(){
+		Collections.sort(lista);
+		
 		String saida = "";
 		int i = 1;
 		for (Musicas m : lista) {
-
 			saida += "Música: #" + (i++) + "\n";
-			saida += m.visualizar1() + "\n";
+			saida += m.visualizar1() +"\n";
 		}
-		return saida;
+			return saida;
 	}
 
-//-----------------------------------------------------------------	
-	static public String listarPlaylists() {
-
-		String saida = "";
-		int i = 1;
-		int e = 1;
-		for (Playlists p : playlists) {
-			saida += "Playlist #" + (i++) + "\n";
-			saida += p.visualizar2() + "\n";
-			for (Musicas m : lista) {
-				saida += "#" + (e++) + "- " + m.visualizar1() + "\n";
-				break;
-			}
-		}
-		return saida;
-	}
-
-//----------------Imprime pelo genero------------------------------
+//----------------Imprime pelo genero------------------------------ok
 	static public String pesquisar(String genero) {
 		int qtd = 0;
 		int i = 1;
 		String saida = "";
-		for (Musicas m : lista) {
+		for (Musicas m : lista){
 			if (m.getGenero().equalsIgnoreCase(genero)) {
 				saida += "Música #" + (i++) + "\n";
-				saida += m.visualizar1() + "\n";
-				qtd++;
-			}
+				saida += m.visualizar1() +"\n";
+				qtd ++;
 		}
-		return saida;
 	}
-
-//-----------------Imprime pelas bandas----------------------------------
+		return saida;
+}
+//-----------------Imprime pelas bandas----------------------------------ok
 	static public String pesquisar1(String banda) {
 		int qtd = 0;
 		int i = 1;
 		String saida = "";
-		for (Musicas m : lista) {
+		for (Musicas m : lista){
 			if (m.getBanda().equalsIgnoreCase(banda)) {
 				saida += "Música #" + (i++) + "\n";
-				saida += m.visualizar1() + "\n";
-				qtd++;
-			}
+				saida += m.visualizar1() +"\n";
+				qtd ++;
 		}
-		return saida;
 	}
-
-//-----------------Imprime pelo ano-------------------------------------
-	static public String pesquisar3(int ano) {
+		return saida;
+}
+//-----------------Imprime pelo ano-------------------------------------ok
+	static public String pesquisar3(String ano) {
 		int qtd = 0;
 		int i = 1;
 		String saida = "";
-		for (Musicas m : lista) {
-			if (m.getAno(ano)) {
+		for (Musicas m: lista){
+			if (m.getAno().equalsIgnoreCase(ano)) {
 				saida += "Música #" + (i++) + "\n";
-				saida += m.visualizar1() + "\n";
-				qtd++;
-			}
+				saida += m.visualizar1() +"\n";
+				qtd ++;
 		}
+	}
 		return saida;
-	}
-
-//------------------------------------------------------------------------------
-	public static void adicionarMusica1(Playlists nomeMusica) {
-		playlists.add(nomeMusica);
-
-	}
-
-//-------------------------------------------------------------------------------
+}
+//-------------------Excluir músicas---------------------------------------------ok
 	static public boolean excluirNome(String nomeMusica) {
-
+		
 		for (Musicas m : lista) {
 			if (m.getNomeMusica().equalsIgnoreCase(nomeMusica)) {
 				lista.remove(m);
@@ -143,15 +99,39 @@ public class ListaMusicas {
 		}
 		return false;
 	}
-
-	public static void excluirInicio() {
-		lista.remove(0);
-	}
-
+	public static void excluirInicio() {		
+			lista.remove(0);			
+}
 	public static void excluirFim() {
-		Musicas m = lista.get(lista.size() - 1);
-		{
+		Musicas m = lista.get(lista.size() - 1);{
 			lista.remove(m);
 		}
 	}
-}
+//------------------------------------------------------------------------------------
+	static public int Sorteio(Playlists playlistNova){
+		ArrayList Sorteio = new ArrayList<>();
+		
+		Collections.shuffle(lista);
+		lista.add(playlistNova);
+		System.out.println("111"+playlistNova);
+	    System.out.println(lista.toString()+"****");
+	    return playlistNova;
+	    
+	   // Random lista = new Random();
+	  //  ListaMusicas lista = new ListaMusicas();
+	    
+	  //  int NumSorteado;
+	}
+	
+	    static public int getListaMusicasSize(){
+			return lista.size();
+		}
+	
+	}
+
+
+	
+
+	
+
+

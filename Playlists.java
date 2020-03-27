9a5c4f7cd543708233c867028c41becca6363e72
Luan Reinheimer <luanmr00@gmail.com>
2020-03-27 18:06:59 -0300

    Alterado alguns metodos dentro da classe Playlists.

--- Playlists.java
+++ Playlists.java
@@ -1,26 +1,41 @@
 package projeto_integrador_UCS;
 
-public class Playlists extends Musicas {
 
+public class Playlists extends Musicas{
+	
 	public String nomePlaylist;
-
-	public Playlists() {
+	
+	public Playlists(){
+		super();
 	}
-
+		
 	public Playlists(String nomePlaylist) {
 		this.nomePlaylist = nomePlaylist;
 	}
-
 //--------------------------------------------------------------
 	public void setNomePlaylist(String nomePlaylist) {
 		this.nomePlaylist = nomePlaylist;
 	}
-
-	public String getNomePlaylist() {
+	public String getNomePlaylist(){
 		return this.nomePlaylist;
 	}
-
+	
 	public String visualizar2() {
-		return nomePlaylist;
+		return nomePlaylist + nomeMusica;	
 	}
+
+	public String visualizar1(String nomeMusica) {
+	
+		return nomeMusica;
+	}
+
+	public void adicionarInicio(String nomeMusica) {
+	
+	}
+
 }
+	
+
+
+	
+

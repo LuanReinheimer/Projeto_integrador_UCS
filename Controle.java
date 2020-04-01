7c7ee2225339e16ee756b3e7c4742c2a2a4367e0
Joao <j.j.shady@hotmail.com>
2020-04-01 18:09:39 -0300

    atualizado código da classe controle

--- Controle.java
+++ Controle.java
@@ -1,47 +1,48 @@
 package projeto_integrador_UCS;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
+
 public class Controle {
-	
-	
+		
 	@SuppressWarnings({ "resource", "unused" })
 	public static void main(String[] args) {
 	
 		Scanner entrada = new Scanner(System.in);
 		Scanner entradaString = new Scanner(System.in);
+
+		Musicas musicaNova = null;
+		String nomeMusica = null, cantor = null, banda = null, album = null, genero = null;
+		String ano = null;
 		
-		int menu;	
-		int numMusicas;
-		
-		Musicas musicaNova;
-		String nomeMusica, cantor, banda, album, genero;
-		String ano;
-		
-		Playlists playlistNova;
+		Playlists playlistNova = null;
 		String nomePlaylist;
 		
+		int menu;
+		
 		do {
-			mostrarMenu();
+			mostrarMenu();			
 			menu = entrada.nextInt();
 			switch(menu) {
 			
 //----------Cadastra as musicas na lista-----------------------------------
-			case 1:			
-					
-					System.out.print("Nome da música? ");
+			case 1:					
+					System.out.print("Nome da música: ");
 					nomeMusica = entradaString.nextLine();
-					System.out.print("Nome do cantor/cantora? ");
+					System.out.print("Nome do cantor/cantora: ");
 					cantor = entradaString.nextLine();
-					System.out.print("Nome da banda/grupo? ");
+					System.out.print("Nome da banda/grupo: ");
 					banda = entradaString.nextLine();
-					System.out.print("Nome do Album? ");
+					System.out.print("Nome do Album: ");
 					album = entradaString.nextLine();
-					System.out.print("Ano de lançamento? "); 
+					System.out.print("Ano de lançamento: "); 
 					ano = entradaString.nextLine();
-					System.out.print("Genero? ");
+					System.out.print("Genero: ");
 					genero = entradaString.nextLine();
 					musicaNova = new Musicas(nomeMusica, cantor, banda, album, ano, genero);
-		
+				//	ListaMusicas.salvaMusicas(musicaNova);
+					
 					ListaMusicas.adicionarMusica(musicaNova);
 					System.out.println("\n"+"-----MÚSICA ADICIONADA-----");
 					break;				
@@ -50,12 +51,12 @@ public class Controle {
 				if(ListaMusicas.getListaMusicasSize() == 0) {
 					System.out.println("\n"+"-----LISTA VAZIA----");
 				}else {
-				System.out.println("\n"+"-----LISTA DE MUSICAS----");
-				for(int i = 0; i < ListaMusicas.getListaMusicasSize(); i++) {
-					System.out.print("**"+ ListaMusicas.tamanho + (i) + "??" + "\n");
+				System.out.println("\n"+"-----LISTA DE MUSICAS----\n");
+				for(int i = 0; i < ListaMusicas.getListaMusicasSize(); i++) {				
 				}
-				}
+				System.out.print("A lista possui " + ListaMusicas.getListaMusicasSize() + " músicas!\n");
 				System.out.println("\n" + ListaMusicas.listarMusicas());
+				}
 				break;
 //----------Excluir musicas----------------------------------------------			
 			case 3:	
@@ -123,63 +124,58 @@ public class Controle {
 
 					System.out.print("\n"+"Nome da Playlist:         ");
 					nomePlaylist = entradaString.nextLine();
-					playlistNova = new Playlists(nomePlaylist);
-				//	ListaPlaylists.adicionarPlaylists(playlistNova);
-					
+					playlistNova = new Playlists(nomePlaylist, nomeMusica, cantor, banda, album, ano, genero);
+					ListaPlaylists.adicionarPlaylists(playlistNova);
 					int resposta = 1;
-					do{
-					//	playlistNova.getNomePlaylist().isEmpty();
-						System.out.println("Qual música quer incluir?***");	
+					do {	
 						
-						nomeMusica = entradaString.nextLine();	
-						musicaNova = new Musicas(nomeMusica);
-						ListaPlaylist.adicionarMusica(playlistNova);
-						//playlistNova.adicionarInicio(nomeMusica);
+						System.out.print("Nome da música: ");
+						nomeMusica = entradaString.nextLine();
+						musicaNova = new Musicas(nomeMusica, cantor, banda, album, ano, genero);					
+						ListaMusicas.adicionarPlaylists(musicaNova);									
 						System.out.print("\n"+"Adicionar música?" 
 						+"\n"+ "SIM = 1 / NÃO = 2: ");
 						resposta = Integer.parseInt(entradaString.nextLine());
 						}while(resposta==1);
-						ListaPlaylist.adicionarPlaylists(playlistNova);
+						
 							System.out.print("\n"+"---Músicas adicionadas na Playlist!---");
-							System.out.println("\n"+"!!PLAYLIST CONCLUÍDA!!");
-					   
+							System.out.println("\n"+"!!PLAYLIST CONCLUÍDA!!");					   
 					break;
 				case 2:
 					System.out.print("\n"+"Nome da Playlist:         ");
 					nomePlaylist = entradaString.nextLine();
-					playlistNova = new Playlists(nomePlaylist);
-					ListaPlaylist.adicionarPlaylists(playlistNova);
-					System.out.println("Quantas músicas adicionar: ");
-					numMusicas = entrada.nextInt();
-					ListaMusicas.Sorteio(playlistNova);					
+					playlistNova = new Playlists(nomePlaylist, nomeMusica, cantor, banda, album, ano, genero);										
+					int numero;
+					System.out.println("Quantas músicas adicionar: ");					
+					numero = entrada.nextInt();
+					ListaMusicas.sorteio();	
+					ListaPlaylists.adicionarPlaylists(playlistNova);
+					ListaPlaylists.adicionarMusica1();
 					break;
 				}
 				break;
 //---------------------------Imprime as playlists de músicas-----------------------------------
 			case 8:
-				System.out.println("-----LISTA DE Playlists----");
-				System.out.println(ListaPlaylist.listarPlaylists());
+				System.out.println("\n"+"-----LISTA DE PLAYLISTS-----\n");
+				System.out.println(ListaPlaylists.listarPlaylists());
 				break;				
 			default:
 				System.out.println("\nOpção inválida!\n");
 				mostrarMenu();
-			}
-		
+			}		
 		}while(menu != 9);
-
 }
 	static void mostrarMenu() {
-		System.out.print("\nDIGITE UMA OPÇÃO?" 
-				+ "\n1- Adicionar músicas" 
-				+ "\n2- Ver Lista de músicas" 
-				+ "\n3- Excluir música" 
-				+ "\n4- Visualizar por genero" 
-				+ "\n5- Visualizar por banda"
-				+ "\n6- Visualizar por ano" 
-				+ "\n7- Criar playlists"   // duas maneira de criar
-				+ "\n8- Visualizar playlists"
-				+ "\n9- Sair" 
+		System.out.print("\n  DIGITE UMA OPÇÃO!!\n" 
+				+ "\n1- ADICIONAR MÚSICAS" 
+				+ "\n2- LISTA DE MÚSICAS" 
+				+ "\n3- EXCLUIR MÚSICA" 
+				+ "\n4- VISUALIZAR GENEROS" 
+				+ "\n5- VISUALIZAR BANDAS/GRUPOS"
+				+ "\n6- VISUALIZAR ANO LANÇAMENTO" 
+				+ "\n7- CRIAR MINHA PLAYLIST"  
+				+ "\n8- VISUALIZAR PLAYLISTS"
+				+ "\n9- SAIR" 
 				+ "\nDIGITE AQUI: ");
 	}
 }
-
